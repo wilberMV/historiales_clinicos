0eeb709d4accbf960b429ce17a323b7b2b86bf58
wilberMV <darkes12345678@gmail.com>
2017-01-18 11:16:21 -0500

    segundo commit

--- src/historialesclinicos/Paciente.java
+++ src/historialesclinicos/Paciente.java
@@ -10,5 +10,64 @@ package historialesclinicos;
  * @author TOSHIBA
  */
 public class Paciente {
+    private String Nombre;
+    private String FechadeNaciomiento;
+    private String InformeConyugal;
+    private int Edad;
+    private String Genero;
+
+    public Paciente() {
+    }
+
+    public Paciente(String Nombre, String FechadeNaciomiento, String InformeConyugal, int Edad, String Genero) {
+        this.Nombre = Nombre;
+        this.FechadeNaciomiento = FechadeNaciomiento;
+        this.InformeConyugal = InformeConyugal;
+        this.Edad = Edad;
+        this.Genero = Genero;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getFechadeNaciomiento() {
+        return FechadeNaciomiento;
+    }
+
+    public void setFechadeNaciomiento(String FechadeNaciomiento) {
+        this.FechadeNaciomiento = FechadeNaciomiento;
+    }
+
+    public String getInformeConyugal() {
+        return InformeConyugal;
+    }
+
+    public void setInformeConyugal(String InformeConyugal) {
+        this.InformeConyugal = InformeConyugal;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+    
+    
+    
     
 }
